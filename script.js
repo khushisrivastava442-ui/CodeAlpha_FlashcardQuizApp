852678cd59fd7b7c7748779b3318e66c4e81032b
Khushi Srivastava <khushisrivastava442@gmail.com>
2026-07-30 17:35:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-body {
-    text-align: center;
-    font-family: Arial;
-    margin-top: 50px;
-}
 
-.card {
-    border: 2px solid black;
-    padding: 20px;
-    width: 300px;
-    margin: auto;
-}
-
-button {
-    padding: 10px;
-    margin-top: 20px;
-}
